ef86576c480a809f12349c894822b6d4b4aa5c63
Flavio Garcia <piraz@candango.org>
2015-12-23 15:54:31 -0500

    Added initial post model.

---
+++ pequi/models.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#
+# Copyright 2015-2016 Flavio Garcia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4:
+
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy.types import Date, Integer, Text, Boolean, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.schema import Index, DefaultClause
+from sqlalchemy.dialects.postgres import TIMESTAMP
+
+from firenado.util.sqlalchemy_util import Base
+
+
+class PostBase(Base):
+
+    __tablename__ = 'posts'
+
+    id = Column('id', Integer(), primary_key=True)
+
+    guid = Column('guid', String(255), nullable=False)
+
+    content = Column('content', Text(), nullable=False)
+
+    title = Column('title', Text(), nullable=False)
+
+    excerpt = Column('excerpt', Text(), nullable=False)
+
+    created = Column('created', TIMESTAMP(), nullable=False)
+
+    updated = Column('updated', TIMESTAMP(), nullable=False)
