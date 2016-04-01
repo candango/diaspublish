b4a87da3babbebfb96a84ac184139810f8d9e61c
Flavio Garcia <piraz@candango.org>
2016-03-31 20:36:27 -0400

    Bumping up firenado.

--- pequi/models.py
+++ pequi/models.py
@@ -13,8 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4:
 
 from sqlalchemy import Column, ForeignKey
 from sqlalchemy.types import Date, Integer, Text, Boolean, String
